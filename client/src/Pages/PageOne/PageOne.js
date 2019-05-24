ae6fa0c711dc425e47763788cdc92d2481b494b1
Joshua Taguicana <joshtaguicana@gmail.com>
2019-05-24 13:49:14 -0400

    iframes for graph added

--- client/src/Pages/PageOne/PageOne.js
+++ client/src/Pages/PageOne/PageOne.js
@@ -4,7 +4,12 @@ export class PageOne extends Component {
     render() {
         return (
             <div>
-                PAGE 1
+                {/* <img src="https://public.tableau.com/static/images/Br/Brainstation3D-Kickstarter/GenreofFilmbySuccessRate/1.png" /> */}
+
+                <iframe src="https://public.tableau.com/profile/lauren.taylor3432#!/vizhome/Brainstation3D-Kickstarter/GenreofFilmbySuccessRate?:showVizHome=n&:embed=true&:display_count=yes"
+                width="600" height="300"></iframe>
+
+                {/* <div className='tableauPlaceholder' id='viz1558718333836' style={{"position": "relative"}}><noscript><a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Br&#47;Brainstation3D-Kickstarter&#47;GenreofFilmbySuccessRate&#47;1_rss.png' style={{"border": "none"}} /></a></noscript><object className='tableauViz'  style={{"display":"none"}}><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Brainstation3D-Kickstarter&#47;GenreofFilmbySuccessRate' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Br&#47;Brainstation3D-Kickstarter&#47;GenreofFilmbySuccessRate&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='filter' value='publish=yes' /></object></div> */}
             </div>
         )
     }
