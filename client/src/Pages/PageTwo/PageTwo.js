6bb1ea83824f4447b5d9bf4d10851840ce10d7b2
Danbi Kim <danbikim@Danbis-Air.localdomain>
2019-05-24 16:47:22 -0400

    u

--- client/src/Pages/PageTwo/PageTwo.js
+++ client/src/Pages/PageTwo/PageTwo.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react'
 import Dropdown from 'react-dropdown'
 import 'react-dropdown/style.css'
+import Compone from '../../components/compone'
 import Comptwo from '../../components/comptwo'
-
-const options = [
-    'Success Rate Duration', 'Category 2', 'Category 3'
-  ]
-
+import Compthree from '../../components/compthree'
+import './PageTwo.scss'
 export class PageTwo extends Component {
     render() {
         return (
-            <div>
-                <Dropdown options={options} onChange={this._onSelect} placeholder="Select a category" />
-                <div>
-                <   Comptwo /> 
+            <div className="container">
+                <div className="buttons">
+                    <button className="buttons__click" onClick={this.displayOne}>
+                        Top Ten Categories
+                    </button>
+                    <Compone /> 
+                    
+                    <button className="buttons__click" onClick={this.displayTwo}>
+                        Genre of Film by Success rate /duration
+                    </button>
+                    <Comptwo /> 
+                    <button className="buttons__click" onClick={this.displayThree}>
+                        Average amount pledged for successful campaigns
+                    </button>
+                    <Compthree /> 
                 </div>
             </div>
         )
     }
 }
-
 export default PageTwo;
