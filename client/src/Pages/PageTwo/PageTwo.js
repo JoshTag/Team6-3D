1b1dc3b91551c9d5d6f05b78eafdedc609314a7f
Danbi Kim <danbikim@Danbis-Air.localdomain>
2019-05-24 16:39:12 -0400

    drop down

--- client/src/Pages/PageTwo/PageTwo.js
+++ client/src/Pages/PageTwo/PageTwo.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Dropdown from 'react-dropdown'
+//import Dropdown from 'react-dropdown'
 import 'react-dropdown/style.css'
 import Compone from '../../Components/compone'
 import Comptwo from '../../Components/comptwo'
